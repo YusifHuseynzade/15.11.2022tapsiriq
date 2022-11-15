84080d915df79610a245777647f014ad0aa40c07
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2022-11-16 01:40:56 +0300

    15.11.2022tapsirig
    add

---
+++ 15.11.2022task/11.15.2022task/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _11._15._2022task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult About()
+        {
+            //ViewResult vr = new ViewResult();
+            //vr.ViewName = "About";
+            //return vr;
+            return View();
+        }
+
+        public ViewResult Index()
+        {
+            //ViewResult vr = new ViewResult();
+            //vr.ViewName = "Index";
+            //return vr;
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            //ViewResult vr = new ViewResult();
+            //vr.ViewName = "Contact";
+            //return vr;
+            return View();
+        }
+
+        public ContentResult Content(int id, string name, string surname, string groupno)
+        {
+            ContentResult vr = new ContentResult();
+            vr.Content = $"Info: {id} - {name} - {surname} - {groupno}";
+            return vr;
+        }
+    }
+}
---
+++ 15.11.2022task/11.15.2022task/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _11._15._2022task.Controllers
+{
+    public class StudentController: Controller
+    {
+        public ViewResult Detail(int id, string groupno)
+        {
+            ViewBag.Id = 99;
+            ViewData["id"] = id;
+            TempData["id"] = 50;
+
+            ViewBag.Groupno = groupno;
+            return View();
+        }
+    }
+}
